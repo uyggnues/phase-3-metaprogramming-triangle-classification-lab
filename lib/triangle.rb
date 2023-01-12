9dffdf49c9a62a46d2819e2a0ce3cea377272bde
Seunggyu <seunggyul14@icloud.com>
2023-01-11 18:46:46 -0800

    commit tree d2d72dec686dfcf9cdc69a00ef298657daae1614

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 == side2 && side1 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def zero?
+    [side1, side2, side3].all? {|num| num == 0}
+  end
+
+  def inequality?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+
+  def validate_triangle
+    raise TriangleError unless zero? && inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+# binding.pry
